8e1670aa5f019f5be185af71f7f9bb9c8f562bf9
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2025-10-11 19:33:01 +0200

    .

--- dist/types/Pat.d.ts
+++ dist/types/Pat.d.ts
@@ -1,10 +1,11 @@
 export interface Pat {
+    id: string;
     scopes: string[];
     created_at: Date;
+    description: string;
     masked?: string;
     token?: string;
     last_used_at?: Date;
     expires_at?: Date;
-    description?: string;
 }
 //# sourceMappingURL=Pat.d.ts.map
--- src/types/Pat.ts
+++ src/types/Pat.ts
@@ -1,9 +1,10 @@
 export interface Pat {
+    id: string,
     scopes: string[],
     created_at: Date,
+    description: string
     masked?: string,
     token?: string,
     last_used_at?: Date,
-    expires_at?: Date,
-    description?: string
+    expires_at?: Date
 }
