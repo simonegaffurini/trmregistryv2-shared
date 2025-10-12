76cc136258a7de430129204b6d96382b442a3682
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2025-10-12 14:36:29 +0200

    .

--- dist/consts/PatScopes.js
+++ dist/consts/PatScopes.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PAT_SCOPES = void 0;
 exports.PAT_SCOPES = [
-    { id: 'package:*', description: 'All functions in package' },
+    { id: 'package:all', description: 'All functions in package' },
     { id: 'package:read', description: 'Download and view packages on the registry.' },
     { id: 'package:publish', description: 'Publish packages to the registry.' },
     { id: 'package:deprecate', description: 'Deprecate packages on the registry.' },
