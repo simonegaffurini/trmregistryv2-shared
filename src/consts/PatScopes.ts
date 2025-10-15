bb6b456f900ac142e77ac9d3e1ed38ef7db071e6
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2025-10-15 21:50:36 +0200

    .

--- src/consts/PatScopes.ts
+++ src/consts/PatScopes.ts
@@ -4,5 +4,6 @@ export const PAT_SCOPES: TokenScope[] = [
   { id: 'package:all', description: 'All functions in package' },
   { id: 'package:read', description: 'Download and view packages on the registry.' },
   { id: 'package:publish', description: 'Publish packages to the registry.' },
+  { id: 'package:unpublish', description: 'Publish packages to the registry.' },
   { id: 'package:deprecate', description: 'Deprecate packages on the registry.' },
 ];
