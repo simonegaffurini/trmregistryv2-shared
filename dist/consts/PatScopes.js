2515c795387c7c8313e5b1df51347ad19caa2509
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2025-10-18 14:35:43 +0200

    .

--- dist/consts/PatScopes.js
+++ dist/consts/PatScopes.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PAT_SCOPES = void 0;
 exports.PAT_SCOPES = [
-    { id: 'package:all', description: 'All functions in package' },
+    { id: 'package:all', description: 'All functions in package.' },
     { id: 'package:read', description: 'Download and view packages on the registry.' },
     { id: 'package:publish', description: 'Publish packages to the registry.' },
     { id: 'package:unpublish', description: 'Unpublish packages from the registry.' },
--- src/consts/PatScopes.ts
+++ src/consts/PatScopes.ts
@@ -1,7 +1,7 @@
 import { TokenScope } from "../types/TokenScope";
 
 export const PAT_SCOPES: TokenScope[] = [
-  { id: 'package:all', description: 'All functions in package' },
+  { id: 'package:all', description: 'All functions in package.' },
   { id: 'package:read', description: 'Download and view packages on the registry.' },
   { id: 'package:publish', description: 'Publish packages to the registry.' },
   { id: 'package:unpublish', description: 'Unpublish packages from the registry.' },
